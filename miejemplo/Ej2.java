d55bfa335ec28674ecc02afac047519db2a5c8d3
Roberto Benavente <roberbg@ejemplo.com>
2019-05-13 13:44:32 +0200

    primer commit despues de clonacion

---
+++ miejemplo/Ej2.java
@@ -0,0 +1,18 @@
+
+/**
+ * Dada una cadena invertir la misma y mostrar por pantalla.
+ */
+public class Ej2
+{
+    public static void main()
+    {
+        int cantidad=0,fin=0;
+        String S1="esta oracion debe estar invertida";
+        fin=S1.length()-1;
+        for(int i=fin;i>=0;i--)
+        {
+            System.out.print(S1.charAt(i));
+        }
+        System.out.print("Prueba merge");
+    }
+}
